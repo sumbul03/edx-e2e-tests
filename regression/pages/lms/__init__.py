3c89c03614557319b302018a8040634a4e0d4a9b
sumbul03 <sumbul03@users.noreply.github.com>
2019-09-14 06:07:38 +0500

    Update __init__.py

--- regression/pages/lms/__init__.py
+++ regression/pages/lms/__init__.py
@@ -15,10 +15,10 @@ LMS_BASE_URL = os.environ.get('LMS_BASE_URL', LMS_STAGE_BASE_URL)
 LMS_PROTOCOL = os.environ.get('LMS_PROTOCOL', 'https')
 
 if BASIC_AUTH_USERNAME and BASIC_AUTH_PASSWORD:
-    LOGIN_BASE_URL = '{}://{}:{}@{}'.format(
+    LOGIN_BASE_URL = 'http://localhost:18000/login'.format(
         LMS_PROTOCOL, BASIC_AUTH_USERNAME, BASIC_AUTH_PASSWORD, LMS_BASE_URL
     )
 else:
-    LOGIN_BASE_URL = '{}://{}'.format(LMS_PROTOCOL, LMS_BASE_URL)
+    LOGIN_BASE_URL = 'http://localhost:18000/login'.format(LMS_PROTOCOL, LMS_BASE_URL)
 
 LMS_REDIRECT_URL = 'https://stage.edx.org'
