d7e8b383fc808518c6e334bd577e6ae55b9da6f3
sumbul03 <sumbul03@users.noreply.github.com>
2019-09-17 20:19:48 +0500

    Update login_lms.py

--- regression/pages/lms/login_lms.py
+++ regression/pages/lms/login_lms.py
@@ -11,22 +11,21 @@ class LmsLogin(LoginPage):
     This class is an extended class of LoginPage,
     where we add methods that are different or not used in LoginPage
     """
-    #url = LOGIN_BASE_URL + '/login'
     url = 'http://edx.devstack.lms:18000' + '/login'
 
     def is_browser_on_page(self):
         """
         Verifies if the browser is on the correct page
         """
-        return self.q(css='.login-button').visible
+        return self.q(css='.js-login.login-button').visible
 
     def provide_info(self, email, password):
         """
         Fill in login info
         'Username' and 'Password' are the user's credentials
         """
-        email_selector = 'input#email'
-        password_selector = 'input#password'
+        email_selector = 'input#login-email'
+        password_selector = 'input#login-password'
 
         self.wait_for_element_visibility(
             email_selector, 'Email input area present')
@@ -52,6 +51,31 @@ class LmsLogin(LoginPage):
         """
         Clicks Remember Me checkbox
         """
-        self.q(css='#remember-yes').click()
+        self.q(css='#login-remember').click()
         # Click initiates an ajax call
         self.wait_for_ajax()
+
+    def send_forgot_password(self, email):
+        """
+        Send forget password email.
+        Arguments:
+             email(str): Email to send forget password to.
+        """
+        self.q(css='.forgot-password.field-link').click()
+        self.wait_for_element_visibility(
+            '#password-reset', 'Wait for Reset Password form'
+        )
+        self.q(css='#password-reset-email').fill(email)
+        self.q(css='.action.action-primary.action-update.js-reset').click()
+        self.wait_for_ajax()
+
+    @property
+    def is_password_reset_email_message_visible(self):
+        """
+        Verify that message for password reset email is visible
+        Returns:
+            bool: True if message is visible.
+        """
+        return self.q(
+            css='.message-title'
+        ).filter(lambda elem: elem.text == 'Check Your Email').visible
