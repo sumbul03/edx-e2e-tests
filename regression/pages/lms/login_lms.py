065e0e097a5364d13aa2a9567d27e445eec62fcb
sumbul03 <sumbul03@users.noreply.github.com>
2019-09-18 03:28:24 +0500

    Update login_lms.py

--- regression/pages/lms/login_lms.py
+++ regression/pages/lms/login_lms.py
@@ -45,11 +45,11 @@ class LmsLogin(LoginPage):
         self.q(css='.login-button').first.click()
 
         # The next page is the dashboard; make sure it loads
-        dashboard = DashboardPage(self.browser)
-        time.sleep(30)
-        dashboard.wait_for_page()
-        
-        return dashboard
+        #dashboard = DashboardPage(self.browser)
+        time.sleep(45)
+        #dashboard.wait_for_page()
+        return self.q(css='#showAccomplishedCourses').visible
+        #return dashboard
 
     def click_remember_me(self):
         """
