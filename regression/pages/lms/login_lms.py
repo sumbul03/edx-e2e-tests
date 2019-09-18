9b0e4856a5d2c1614808f0b219af727219f51536
sumbul03 <sumbul03@users.noreply.github.com>
2019-09-18 23:24:11 +0500

    Update login_lms.py

--- regression/pages/lms/login_lms.py
+++ regression/pages/lms/login_lms.py
@@ -4,6 +4,7 @@ LMS login page
 from edxapp_acceptance.pages.lms.login import LoginPage
 from edxapp_acceptance.pages.lms.dashboard import DashboardPage
 from regression.pages.lms import LOGIN_BASE_URL
+from regression.pages.lms.dashboard_lms import DashboardPageExtended
 import time
 
 
@@ -45,11 +46,12 @@ class LmsLogin(LoginPage):
         self.q(css='.login-button').first.click()
 
         # The next page is the dashboard; make sure it loads
-        dashboard = DashboardPage(self.browser)
-        #time.sleep(45)
-        dashboard.wait_for_page(90)
-        dashboard = 'http://edx.devstack.lms:18000/dashboard'
+        dashboard = DashboardPageExtended(self.browser)
         
+        dashboard.wait_for_page(90)
+        #dashboard = 'http://edx.devstack.lms:18000/dashboard'
+        dashtest = self.q(css='.my-courses').present
+        print("Dashtest is " + dashtest)
         #return self.q(css='.my-courses').present
         return dashboard
 
