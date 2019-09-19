85985f5150213517bccf8336520ca9517536c5ea
sumbul03 <sumbul03@users.noreply.github.com>
2019-09-19 21:31:46 +0500

    Update test_lms_login.py

--- regression/tests/lms/test_lms_login.py
+++ regression/tests/lms/test_lms_login.py
@@ -35,10 +35,7 @@ class LoginTest(WebAppTest):
         time.sleep(45)
         self.login_page.login(self.DEMO_COURSE_USER, self.DEMO_COURSE_PASSWORD)
         time.sleep(45)
-                
-        self.dashboard_ext.visit()
-        time.sleep(90)
-        
+    
         all_co = self.login_page.q(
                 css='.my-courses').present
         print(all_co)
